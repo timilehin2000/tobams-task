4d4d094afa3bbe212f285f1df6d7d5ca377338ba
Timilehin Abodunrin <joeltimmy7@gmail.com>
2023-08-01 22:05:43 +0100

    Update dashboard.controller.js

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -3,6 +3,7 @@ const { capitalizeFirstLetter } = require("../helpers/utils");
 
 const dashboard = (req, res) => {
     const { userName } = req.user;
+    console.log(req.user)
 
     return res.json({
         data: `Welcome to your dashboard ${capitalizeFirstLetter(userName)}`,
